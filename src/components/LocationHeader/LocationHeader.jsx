ffe846756640affe163315bb15c42bb4cf3c0ed9
Murad Gojayev <muradqocayevi20@gmail.com>
2023-03-30 12:11:53 +0400

    header completed

---
+++ src/components/LocationHeader/LocationHeader.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const LocationHeader = () => {
+  return (
+    <div className='locationheader'>
+      <div className="container">
+        <div className="locationheader--content">
+          
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default LocationHeader
