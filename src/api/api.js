d733320da15e4f2955cae0ae804b271bb5a301fa
Murad Gojayev <muradqocayevi20@gmail.com>
2023-04-03 23:03:04 +0400

    api class

---
+++ src/api/api.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+let baseUrl = "http://localhost:3000/";
+
+export default class FetchData {
+  static async getData(location) {
+    return await axios.get(baseUrl + location);
+  }
+  static async addData(location, data) {
+    data = { backet: data }; 
+    return await axios.patch(baseUrl + location, data);
+  }
+}
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from "react";
 import "./home.scss";
+import FetchData from "../../api/api";
 
 // import components
 
 import ProductsList from "../../components/ProductsList/ProductsList";
 
-// import our brand images
+//#region our brand images
 import brand1 from "../../assets/home_page/brand-1.png";
 import brand2 from "../../assets/home_page/brand-2.png";
 import brand3 from "../../assets/home_page/brand-3.png";
@@ -19,6 +20,8 @@ import brand10 from "../../assets/home_page/brand-10.png";
 import brand11 from "../../assets/home_page/brand-11.png";
 import brand12 from "../../assets/home_page/brand-12.png";
 
+//#endregion images
+
 import introImg from "../../assets/flash-sale-ads.png";
 import { NavLink } from "react-router-dom";
 import Loading from "../../components/Loading/Loading";
@@ -27,23 +30,22 @@ const Home = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [popularSales, setPopularSales] = useState([]);
 
-  useEffect(() => {
-    fetch("http://localhost:3000/products")
-      .then((res) => res.json())
-      .then((data) => setPopularSales(data));
+  async function getPopularSales() {
+    const response = await FetchData.getData("products");
+    setPopularSales(response.data);
+  }
 
+  useEffect(() => {
+    getPopularSales();
     setTimeout(() => {
       setIsLoading(false);
     }, 1000);
   }, []);
 
-  
-
   return (
     <>
+      <Loading isLoading={isLoading} />
 
-    <Loading isLoading={isLoading}/>
-    
       <div className="intro">
         <div className="container">
           <div className="intro--content">
@@ -72,35 +74,35 @@ const Home = () => {
                   d="M1 1H5.63636V24.1818H35"
                   stroke="#FFBB38"
                   strokeWidth="2"
-                  stroke-miterlimit="10"
+                  strokeMiterlimit="10"
                   strokeLinecap="square"
                 ></path>
                 <path
                   d="M8.72763 35.0002C10.4347 35.0002 11.8185 33.6163 11.8185 31.9093C11.8185 30.2022 10.4347 28.8184 8.72763 28.8184C7.02057 28.8184 5.63672 30.2022 5.63672 31.9093C5.63672 33.6163 7.02057 35.0002 8.72763 35.0002Z"
                   stroke="#FFBB38"
                   strokeWidth="2"
-                  stroke-miterlimit="10"
+                  strokeMiterlimit="10"
                   strokeLinecap="square"
                 ></path>
                 <path
                   d="M31.9073 35.0002C33.6144 35.0002 34.9982 33.6163 34.9982 31.9093C34.9982 30.2022 33.6144 28.8184 31.9073 28.8184C30.2003 28.8184 28.8164 30.2022 28.8164 31.9093C28.8164 33.6163 30.2003 35.0002 31.9073 35.0002Z"
                   stroke="#FFBB38"
                   strokeWidth="2"
-                  stroke-miterlimit="10"
+                  strokeMiterlimit="10"
                   strokeLinecap="square"
                 ></path>
                 <path
                   d="M34.9982 1H11.8164V18H34.9982V1Z"
                   stroke="#FFBB38"
                   strokeWidth="2"
-                  stroke-miterlimit="10"
+                  strokeMiterlimit="10"
                   strokeLinecap="square"
                 ></path>
                 <path
                   d="M11.8164 7.18164H34.9982"
                   stroke="#FFBB38"
                   strokeWidth="2"
-                  stroke-miterlimit="10"
+                  strokeMiterlimit="10"
                   strokeLinecap="square"
                 ></path>
               </svg>
@@ -121,13 +123,13 @@ const Home = () => {
                   d="M31 17.4502C31 25.7002 24.25 32.4502 16 32.4502C7.75 32.4502 1 25.7002 1 17.4502C1 9.2002 7.75 2.4502 16 2.4502C21.85 2.4502 26.95 5.7502 29.35 10.7002"
                   stroke="#FFBB38"
                   strokeWidth="2"
-                  stroke-miterlimit="10"
+                  strokeMiterlimit="10"
                 ></path>
                 <path
                   d="M30.7 2L29.5 10.85L20.5 9.65"
                   stroke="#FFBB38"
                   strokeWidth="2"
-                  stroke-miterlimit="10"
+                  strokeMiterlimit="10"
                   strokeLinecap="square"
                 ></path>
               </svg>
@@ -148,21 +150,21 @@ const Home = () => {
                   d="M22.6654 18.667H9.33203V27.0003H22.6654V18.667Z"
                   stroke="#FFBB38"
                   strokeWidth="2"
-                  stroke-miterlimit="10"
+                  strokeMiterlimit="10"
                   strokeLinecap="square"
                 ></path>
                 <path
                   d="M12.668 18.6663V13.6663C12.668 11.833 14.168 10.333 16.0013 10.333C17.8346 10.333 19.3346 11.833 19.3346 13.6663V18.6663"
                   stroke="#FFBB38"
                   strokeWidth="2"
-                  stroke-miterlimit="10"
+                  strokeMiterlimit="10"
                   strokeLinecap="square"
                 ></path>
                 <path
                   d="M31 22C31 30.3333 24.3333 37 16 37C7.66667 37 1 30.3333 1 22V5.33333L16 2L31 5.33333V22Z"
                   stroke="#FFBB38"
                   strokeWidth="2"
-                  stroke-miterlimit="10"
+                  strokeMiterlimit="10"
                   strokeLinecap="square"
                 ></path>
               </svg>
@@ -183,32 +185,32 @@ const Home = () => {
                   d="M7 13H5.5C2.95 13 1 11.05 1 8.5V1H7"
                   stroke="#FFBB38"
                   strokeWidth="2"
-                  stroke-miterlimit="10"
+                  strokeMiterlimit="10"
                 ></path>
                 <path
                   d="M25 13H26.5C29.05 13 31 11.05 31 8.5V1H25"
                   stroke="#FFBB38"
                   strokeWidth="2"
-                  stroke-miterlimit="10"
+                  strokeMiterlimit="10"
                 ></path>
                 <path
                   d="M16 28V22"
                   stroke="#FFBB38"
                   strokeWidth="2"
-                  stroke-miterlimit="10"
+                  strokeMiterlimit="10"
                 ></path>
                 <path
                   d="M16 22C11.05 22 7 17.95 7 13V1H25V13C25 17.95 20.95 22 16 22Z"
                   stroke="#FFBB38"
                   strokeWidth="2"
-                  stroke-miterlimit="10"
+                  strokeMiterlimit="10"
                   strokeLinecap="square"
                 ></path>
                 <path
                   d="M25 34H7C7 30.7 9.7 28 13 28H19C22.3 28 25 30.7 25 34Z"
                   stroke="#FFBB38"
                   strokeWidth="2"
-                  stroke-miterlimit="10"
+                  strokeMiterlimit="10"
                   strokeLinecap="square"
                 ></path>
               </svg>
