6d5f5418f321f4253559326fdbbff76936085863
Murad Gojayev <muradgojayevv@gmail.com>
2023-07-06 11:50:07 +0400

    total price

--- src/layouts/MainLayout/MainLayout.js
+++ src/layouts/MainLayout/MainLayout.js
@@ -5,37 +5,31 @@ import Footer from "./components/footer/Footer";
 import WelcomePopup from "./components/WelcomePopup/WelcomePopup";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { toast } from 'react-hot-toast'
-import { getBasketItemsAsync, getBasketItemsDatasAsync, removeBasketItemsAsync } from "../../redux/basket/basketSlice";
+import { toast } from "react-hot-toast";
+import {
+  getBasketItemsAsync,
+  getBasketItemsDatasAsync,
+  removeBasketItemsAsync,
+} from "../../redux/basket/basketSlice";
 import { getFavoritesProductsAsync } from "../../redux/favorites/favoritesSlice";
 
 const MainLayout = () => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   const [showWelcomePopup, setShowWelcomePopup] = useState(false);
-  const [progress, setProgress] = useState(0)
+  const [progress, setProgress] = useState(0);
   const [sideBasket, setSideBasket] = useState(false);
+  const totalPrice = useSelector((state) => state.basket.totalPrice);
   const products = useSelector((state) => state.basket.productsDetails);
   const loadingProducts = useSelector((state) => state.basket.loading.main);
   const basketItems = useSelector((state) => state.basket.basketItems.data);
-  const [totalPrice, setTotalPrice] = useState(0);
 
   const auth = localStorage.getItem("user");
-
-  const calculateTotalPrice = () => {
-    products.forEach(product => {
-      basketItems.forEach((basketItem) => {
-        if (basketItem.product == product._id) {
-          const itemPrice = product.offer > 0 ? product.offer : product.price
-          setTotalPrice(price => price + (basketItem.count * itemPrice))
-        }
-      })
-    })
-  }
+  console.log(totalPrice);
 
   useEffect(() => {
     if (auth) {
       dispatch(getBasketItemsAsync(toast));
-      dispatch(getFavoritesProductsAsync(toast))
+      dispatch(getFavoritesProductsAsync(toast));
     }
     //#region welcome popup
     const welcomePopup = JSON.parse(localStorage.getItem("welcomePopup"));
@@ -64,87 +58,114 @@ const MainLayout = () => {
       }
     }
     //#endregion
-  }, [])
+  }, []);
   useEffect(() => {
     if (sideBasket && basketItems.length > 0 && auth) {
       dispatch(getBasketItemsDatasAsync(toast));
-      setTotalPrice(0);
     }
-  }, [sideBasket])
+  }, [sideBasket]);
 
   //! reading progress bar
-  window.addEventListener('scroll', updateProgressBar);
+  window.addEventListener("scroll", updateProgressBar);
   function updateProgressBar() {
-    const scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
-    const windowHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    const scrollPosition =
+      document.documentElement.scrollTop || document.body.scrollTop;
+    const windowHeight =
+      document.documentElement.scrollHeight -
+      document.documentElement.clientHeight;
     const progress = (scrollPosition / windowHeight) * 100;
-    setProgress((progress).toFixed(0));
+    setProgress(progress.toFixed(0));
   }
 
   const removeItemHandler = (id) => {
-    dispatch(removeBasketItemsAsync({ productId: id, count: 0 }))
+    dispatch(removeBasketItemsAsync({ productId: id, count: 0 }));
   };
 
   useEffect(() => {
     if (auth && sideBasket) {
-      setTotalPrice(0)
       dispatch(getBasketItemsDatasAsync(toast));
-      calculateTotalPrice();
     }
-  }, [basketItems])
+  }, [basketItems]);
 
   return (
     <>
-      <div onClick={() => window.scrollTo(0, 0)} className={progress > 10 ? 'progress-bar_container active' : 'progress-bar_container'} >
-        <div className="progress-bar" style={{ "--progress-percent": progress, color: 'white' }}>
+      <div
+        onClick={() => window.scrollTo(0, 0)}
+        className={
+          progress > 10
+            ? "progress-bar_container active"
+            : "progress-bar_container"
+        }
+      >
+        <div
+          className="progress-bar"
+          style={{ "--progress-percent": progress, color: "white" }}
+        >
           <i className="fa-solid fa-chevron-up"></i>
         </div>
       </div>
       {/* SideBar Basket */}
-      <div onMouseDown={() => setSideBasket(false)} className="sidebar_basket--overlay" data-visible={sideBasket}></div>
+      <div
+        onMouseDown={() => setSideBasket(false)}
+        className="sidebar_basket--overlay"
+        data-visible={sideBasket}
+      ></div>
       <div data-visible={sideBasket} className="sidebar_basket">
         <div className="sidebar_basket--head">
           <h5>Səbət</h5>
-          <button onClick={() => setSideBasket(false)}>Bağla <i class="fa-solid fa-arrow-right-long"></i></button>
+          <button onClick={() => setSideBasket(false)}>
+            Bağla <i class="fa-solid fa-arrow-right-long"></i>
+          </button>
         </div>
         <div className="content">
-          {loadingProducts && basketItems.length > 0 && <div className="loading"></div>}
-          {!loadingProducts && basketItems.length > 0 &&
+          {loadingProducts && basketItems.length > 0 && (
+            <div className="loading"></div>
+          )}
+          {!loadingProducts &&
+            basketItems.length > 0 &&
             products.map((item, index) => {
-              let count
+              let count;
               basketItems.forEach((basketItem) => {
                 if (basketItem.product == item._id) {
-                  count = basketItem.count
+                  count = basketItem.count;
                   return;
                 }
-              })
-              return <div key={index} className="item">
-                <div className="desc">
-                  <p>{item.name}</p>
-                  <p className="count">{count} X <span className="price">₼{item.offer ? item.offer : item.price}</span></p>
+              });
+              return (
+                <div key={index} className="item">
+                  <div className="desc">
+                    <p>{item.name}</p>
+                    <p className="count">
+                      {count} X{" "}
+                      <span className="price">
+                        ₼{item.offer ? item.offer : item.price}
+                      </span>
+                    </p>
+                  </div>
+                  <img src={item.image[0]} />
+                  <button
+                    onClick={() => removeItemHandler(item._id)}
+                    className="delete"
+                  >
+                    <i class="fa-solid fa-xmark"></i>
+                  </button>
                 </div>
-                <img src={item.image[0]} />
-                <button onClick={() => removeItemHandler(item._id)} className="delete" >
-                  <i class="fa-solid fa-xmark"></i>
-                </button>
-              </div>
-            })
-          }
+              );
+            })}
         </div>
-        {
-          basketItems.length > 0 ? (
-            <div className="actions">
-              <p>Ümumi <span>${totalPrice.toFixed(2)}</span></p>
-              <div className="bottom">
-                <button className="view_cart">Səbətə keç</button>
-                <button className="checkout">Sifariş</button>
-              </div>
+        {basketItems.length > 0 ? (
+          <div className="actions">
+            <p>
+              Ümumi <span>${totalPrice.toFixed(2)}</span>
+            </p>
+            <div className="bottom">
+              <button className="view_cart">Səbətə keç</button>
+              <button className="checkout">Sifariş</button>
             </div>
-          ) : (
-            <p className="empty-cart">Səbətiniz boşdur</p>
-          )
-        }
-
+          </div>
+        ) : (
+          <p className="empty-cart">Səbətiniz boşdur</p>
+        )}
       </div>
       {showWelcomePopup && <WelcomePopup closePopup={setShowWelcomePopup} />}
       <Header setSideBasket={setSideBasket} auth={auth} />
--- src/redux/basket/basketSlice.js
+++ src/redux/basket/basketSlice.js
@@ -1,82 +1,96 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { addToBasket, getBasketItems, getBasketItemsData, removeBasketItems } from "../../api/basket";
+import {
+  addToBasket,
+  getBasketItems,
+  getBasketItemsData,
+  removeBasketItems,
+} from "../../api/basket";
 
 const initialState = {
-    loading: {
-        main: false
-    },
-    basketItems: {
-        data: []
-    },
-    productsDetails: [],
+  loading: {
+    main: false,
+  },
+  basketItems: {
+    data: [],
+  },
+  productsDetails: [],
+  totalPrice: 0,
 };
 
 export const basketSlice = createSlice({
-    name: "basket",
-    initialState,
-    reducers: {
-        setBasketItems: (state, action) => {
-            state.basketItems = {
-                ...state.basketItems,
-                ...action.payload,
-            };
-        },
-        setBasketLoading: (state, action) => {
-            state.loading = { ...state.loading, ...action.payload }
-        },
-        setBasketProductsDetails : (state, action) =>{
-            state.productsDetails = action.payload
-        }
+  name: "basket",
+  initialState,
+  reducers: {
+    setBasketItems: (state, action) => {
+      state.basketItems = {
+        ...state.basketItems,
+        ...action.payload,
+      };
     },
+    setBasketLoading: (state, action) => {
+      state.loading = { ...state.loading, ...action.payload };
+    },
+    setBasketProductsDetails: (state, action) => {
+      state.productsDetails = action.payload;
+    },
+    setTotalPrice: (state, action) => {
+      state.totalPrice = action.payload;
+    },
+  },
 });
 
 export const addToBasketAsync = (data, toast) => async (dispatch) => {
-    dispatch(setBasketLoading({ [data.productId]: true }));
-    try {
-        const response = await addToBasket(data);
-        response && dispatch(setBasketItems({ data: response.data }));
-    } catch (error) {
-        error && toast.error(error.message);
-    }
-    dispatch(setBasketLoading({ [data.productId]: false }));
+  dispatch(setBasketLoading({ [data.productId]: true }));
+  try {
+    const response = await addToBasket(data);
+    response && dispatch(setBasketItems({ data: response.data }));
+  } catch (error) {
+    error && toast.error(error.message);
+  }
+  dispatch(setBasketLoading({ [data.productId]: false }));
 };
 
 export const removeBasketItemsAsync = (data, toast) => async (dispatch) => {
-    dispatch(setBasketLoading({ [data.productId]: true }));
+  dispatch(setBasketLoading({ [data.productId]: true }));
 
-    try {
-        const response = await removeBasketItems(data);
-        response && dispatch(setBasketItems({ data: response.data }))
-    } catch (error) {
-        error && toast.error(error.message)
-    }
-    dispatch(setBasketLoading({ [data.productId]: false }));
-
-}
-
-export const getBasketItemsAsync = (toast) => async (dispatch) => {
-    dispatch(setBasketLoading({ main: true }));
-    try {
-        const response = await getBasketItems();
-        response && dispatch(setBasketItems({ data: response.data }));
-    } catch (error) {
-        error && toast.error(error.message);
-    }
-    dispatch(setBasketLoading({ main: true }));
+  try {
+    const response = await removeBasketItems(data);
+    response && dispatch(setBasketItems({ data: response.data }));
+  } catch (error) {
+    error && toast.error(error.message);
+  }
+  dispatch(setBasketLoading({ [data.productId]: false }));
 };
 
+export const getBasketItemsAsync = (toast) => async (dispatch) => {
+  dispatch(setBasketLoading({ main: true }));
+  try {
+    const response = await getBasketItems();
+    response && dispatch(setBasketItems({ data: response.data }));
+  } catch (error) {
+    error && toast.error(error.message);
+  }
+  dispatch(setBasketLoading({ main: true }));
+};
 
 export const getBasketItemsDatasAsync = (toast) => async (dispatch) => {
-    dispatch(setBasketLoading({ main: true }));
-    try {
-        const response = await getBasketItemsData();
-        response && dispatch(setBasketProductsDetails(response.data))
-    } catch (error) {
-        error && toast.error(error.message);
-    }
-    dispatch(setBasketLoading({ main: false }));
-}
+  dispatch(setBasketLoading({ main: true }));
+  try {
+    const response = await getBasketItemsData();
+    response && dispatch(setBasketProductsDetails(response.data));
+    response && dispatch(setTotalPrice(response.price));
+    console.log(response.price);
+  } catch (error) {
+    error && toast.error(error.message);
+  }
+  dispatch(setBasketLoading({ main: false }));
+};
 
-export const { setBasketItems, setBasketLoading, setBasketProductsDetails } = basketSlice.actions;
+export const {
+  setBasketItems,
+  setBasketLoading,
+  setBasketProductsDetails,
+  setTotalPrice,
+} = basketSlice.actions;
 
 export default basketSlice.reducer;
